4715b29182a4f0b2df035dce464057180d5659e6
nick <nikoschatzoudas@gmail.com>
2026-05-11 12:41:03 +0300

    mobile sizes

--- src/components/apps/PopupMessage.tsx
+++ src/components/apps/PopupMessage.tsx
@@ -4,7 +4,7 @@ const PopupMessage: React.FC = () => {
     return (
         <div style={{ padding: '20px' }}>
             <p>You have been redirected from the mobile version of the site!</p>
-            <p>Welcome to the OS experience!</p>
+            <br />
             <p>Use a desktop or laptop for the full experience.</p>
         </div>
     );
